ff3a0eb26ae6f49d0d271380900aaac039f1a528
eitwizlearn <eit@wizlearn.com>
2013-10-28 16:57:46 +0800

    Update SaveDocument.js
    Customize to call own method in html

--- src/js/SaveDocument.js
+++ src/js/SaveDocument.js
@@ -4,84 +4,89 @@
 * 
 * @constructor
 */
-mindmaps.SaveDocumentView = function() {
-  var self = this;
+mindmaps.SaveDocumentView = function () {
 
-  var $dialog = $("#template-save").tmpl().dialog({
-    autoOpen : false,
-    modal : true,
-    zIndex : 5000,
-    width : 550,
-    close : function() {
-      // remove dialog from DOM
-      $(this).dialog("destroy");
-      $(this).remove();
+    //Original
+    var self = this;
+    var $dialog = $("#template-save").tmpl().dialog({
+        autoOpen: false,
+        modal: true,
+        zIndex: 5000,
+        width: 550,
+        close: function () {
+            // remove dialog from DOM
+            $(this).dialog("destroy");
+            $(this).remove();
+        }
+    });
+
+
+    var $saveCloudStorageButton = $("#button-save-cloudstorage").button().click(
+            function () {
+                if (self.cloudStorageButtonClicked) {
+                    self.cloudStorageButtonClicked();
+                }
+            });
+
+    var $localSorageButton = $("#button-save-localstorage").button().click(
+            function () {
+                if (self.localStorageButtonClicked) {
+                    self.localStorageButtonClicked();
+                }
+            });
+
+    var $autoSaveCheckbox = $("#checkbox-autosave-localstorage").click(
+            function () {
+                if (self.autoSaveCheckboxClicked) {
+                    self.autoSaveCheckboxClicked($(this).prop("checked"));
+                }
+            });
+
+    var $hddSaveButton = $("#button-save-hdd").button().downloadify({
+        filename: function () {
+            if (self.fileNameRequested) {
+                return self.fileNameRequested();
+            }
+        },
+        data: function () {
+            //This is the part where the data is converted to json
+            if (self.fileContentsRequested) {
+                //data is self.fileContentsRequested();
+                alert(self.fileContentsRequested());
+                return self.fileContentsRequested();
+            }
+        },
+        onComplete: function () {
+            if (self.saveToHddComplete) {
+                self.saveToHddComplete();
+            }
+        },
+        onError: function () {
+            console.log("error while saving to hdd");
+        },
+        swf: 'media/downloadify.swf',
+        downloadImage: 'img/transparent.png',
+        width: 65,
+        height: 29,
+        append: true
+    });
+
+    this.setAutoSaveCheckboxState = function (checked) {
+        $autoSaveCheckbox.prop("checked", checked);
     }
-  });
 
+    this.showSaveDialog = function () {
+        $dialog.dialog("open");
+    };
 
-  var $saveCloudStorageButton = $("#button-save-cloudstorage").button().click(
-    function() {
-      if (self.cloudStorageButtonClicked) {
-        self.cloudStorageButtonClicked();
-      }
-    });
+    this.hideSaveDialog = function () {
+        $dialog.dialog("close");
+    };
 
-  var $localSorageButton = $("#button-save-localstorage").button().click(
-    function() {
-      if (self.localStorageButtonClicked) {
-        self.localStorageButtonClicked();
-      }
-    });
+    this.showCloudError = function (msg) {
+        $dialog.find('.cloud-error').text(msg);
+    }
 
-  var $autoSaveCheckbox = $("#checkbox-autosave-localstorage").click(
-    function() {
-      if (self.autoSaveCheckboxClicked) {
-        self.autoSaveCheckboxClicked($(this).prop("checked"));
-      }
-    });
-
-  var $hddSaveButton = $("#button-save-hdd").button().downloadify({
-    filename : function() {
-      if (self.fileNameRequested) {
-        return self.fileNameRequested();
-      }
-    },
-    data : function() {
-      if (self.fileContentsRequested) {
-        return self.fileContentsRequested();
-      }
-    },
-    onComplete : function() {
-      if (self.saveToHddComplete) {
-        self.saveToHddComplete();
-      }
-    },
-    onError : function() {
-      console.log("error while saving to hdd");
-    },
-    swf : 'media/downloadify.swf',
-    downloadImage : 'img/transparent.png',
-    width : 65,
-    height : 29,
-    append : true
-  });
-
-  this.setAutoSaveCheckboxState = function(checked) {
-    $autoSaveCheckbox.prop("checked", checked);
-  }
-
-  this.showSaveDialog = function() {
-    $dialog.dialog("open");
-  };
-
-  this.hideSaveDialog = function() {
-    $dialog.dialog("close");
-  };
-
-  this.showCloudError = function(msg) {
-    $dialog.find('.cloud-error').text(msg);
-  }
 };
 
 /**
@@ -95,92 +100,101 @@ mindmaps.SaveDocumentView = function() {
 * @param {mindmaps.AutoSaveController} autosaveController
 * @param {mindmaps.FilePicker} filePicker
 */
-mindmaps.SaveDocumentPresenter = function(eventBus, mindmapModel, view, autosaveController, filePicker) {
+mindmaps.SaveDocumentPresenter = function (eventBus, mindmapModel, view, autosaveController, filePicker) {
 
-  /**
-  * Save in cloud button was clicked.
-  */
-  view.cloudStorageButtonClicked = function() {
-    mindmaps.Util.trackEvent("Clicks", "cloud-save");
+    /**
+    * Save in cloud button was clicked.
+    */
+    view.cloudStorageButtonClicked = function () {
+        mindmaps.Util.trackEvent("Clicks", "cloud-save");
 
-    filePicker.save({
-      success: function() {
+        filePicker.save({
+            success: function () {
+                view.hideSaveDialog();
+            },
+            error: function (msg) {
+                view.showCloudError(msg);
+            }
+        });
+    };
+
+    /**
+    * View callback when local storage button was clicked. Saves the document
+    * in the local storage.
+    * 
+    * @ignore
+    */
+    view.localStorageButtonClicked = function () {
+        mindmaps.Util.trackEvent("Clicks", "localstorage-save");
+
+        var success = mindmapModel.saveToLocalStorage();
+        if (success) {
+            view.hideSaveDialog();
+        } else {
+            eventBus.publish(mindmaps.Event.NOTIFICATION_ERROR, "Error while saving to local storage");
+        }
+    };
+
+
+    /**
+    * View callback: Enables or disables the autosave function for localstorage.
+    *
+    * @ignore
+    */
+    view.autoSaveCheckboxClicked = function (checked) {
+        if (checked) {
+            autosaveController.enable();
+        } else {
+            autosaveController.disable();
+        }
+    }
+
+    /**
+    * View callback: Returns the filename for the document for saving on hard
+    * drive.
+    * 
+    * @ignore
+    * @returns {String}
+    */
+    view.fileNameRequested = function () {
+        mindmaps.Util.trackEvent("Clicks", "hdd-save");
+
+        return mindmapModel.getMindMap().getRoot().getCaption() + ".json";
+    };
+
+    /**
+    * View callback: Returns the serialized document.
+    * 
+    * @ignore
+    * @returns {String}
+    */
+    view.fileContentsRequested = function () {
+        var doc = mindmapModel.getDocument();
+        return doc.prepareSave().serialize();
+    };
+
+    /**
+    * View callback: Saving to the hard drive was sucessful.
+    * 
+    * @ignore
+    */
+    view.saveToHddComplete = function () {
+        var doc = mindmapModel.getDocument();
+        eventBus.publish(mindmaps.Event.DOCUMENT_SAVED, doc);
         view.hideSaveDialog();
-      },
-      error: function(msg) {
-        view.showCloudError(msg);
-      }
-    });
-  };
+    };
 
-  /**
-  * View callback when local storage button was clicked. Saves the document
-  * in the local storage.
-  * 
-  * @ignore
-  */
-  view.localStorageButtonClicked = function() {
-    mindmaps.Util.trackEvent("Clicks", "localstorage-save");
+    this.go = function () {
+        //Original
+        //view.setAutoSaveCheckboxState(autosaveController.isEnabled());
+        //view.showSaveDialog();
 
-    var success = mindmapModel.saveToLocalStorage();
-    if (success) {
-      view.hideSaveDialog();
-    } else {
-      eventBus.publish(mindmaps.Event.NOTIFICATION_ERROR, "Error while saving to local storage");
-    }
-  };
-
-
-  /**
-  * View callback: Enables or disables the autosave function for localstorage.
-  *
-  * @ignore
-  */
-  view.autoSaveCheckboxClicked = function(checked) {
-    if (checked) {
-      autosaveController.enable();
-    } else {
-      autosaveController.disable();
-    }
-  }
-
-  /**
-  * View callback: Returns the filename for the document for saving on hard
-  * drive.
-  * 
-  * @ignore
-  * @returns {String}
-  */
-  view.fileNameRequested = function() {
-    mindmaps.Util.trackEvent("Clicks", "hdd-save");
-
-    return mindmapModel.getMindMap().getRoot().getCaption() + ".json";
-  };
-
-  /**
-  * View callback: Returns the serialized document.
-  * 
-  * @ignore
-  * @returns {String}
-  */
-  view.fileContentsRequested = function() {
-    var doc = mindmapModel.getDocument();
-    return doc.prepareSave().serialize();
-  };
-
-  /**
-  * View callback: Saving to the hard drive was sucessful.
-  * 
-  * @ignore
-  */
-  view.saveToHddComplete = function() {
-    var doc = mindmapModel.getDocument();
-    eventBus.publish(mindmaps.Event.DOCUMENT_SAVED, doc);
-    view.hideSaveDialog();
-  };
-
-  this.go = function() {
-    view.setAutoSaveCheckboxState(autosaveController.isEnabled());
-    view.showSaveDialog();
-  };
+        //Modified by Eric
+        if (view.fileContentsRequested) {
+            SaveMindMap(view.fileContentsRequested(),"FALSE");
+        }
+        else {
+            alert("There is an error getting the data to save. Please try again.");
+        }
+    };
 };
