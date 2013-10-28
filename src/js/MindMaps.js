2566a0614f716b4e3d8e042c491f94290fa64494
eitwizlearn <eit@wizlearn.com>
2013-10-28 16:55:24 +0800

    Update MindMaps.js
    Remove alert user that you are leaving this page

--- src/js/MindMaps.js
+++ src/js/MindMaps.js
@@ -63,9 +63,9 @@ $(function() {
   setupConsole();
   trackErrors();
 
-  if (!mindmaps.DEBUG) {
-    addUnloadHook();
-  }
+  //if (!mindmaps.DEBUG) {
+  //  addUnloadHook();
+  //}
 
   // create a new app controller and go
   var appController = new mindmaps.ApplicationController();
